340dea4b51ecfee8fdbc549b796a6395ca136a19
Mike <michaxze@gmail.com>
2012-03-29 01:21:49 +0800

    removing logs

--- jobs/app/logs/log-2012-03-27.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2012-03-27 00:19:21 --> Severity: Notice  --> Undefined variable: case /Users/admin/Sites/jobs/app/views/account/cases.php 11
-ERROR - 2012-03-27 00:20:10 --> Severity: Notice  --> Undefined variable: case /Users/admin/Sites/jobs/app/views/account/cases.php 11
-ERROR - 2012-03-27 00:45:23 --> Severity: Notice  --> Undefined index: id /Users/admin/Sites/jobs/app/views/account/cases.php 11
-ERROR - 2012-03-27 00:47:24 --> Severity: Notice  --> A non well formed numeric value encountered /Users/admin/Sites/jobs/app/views/account/cases.php 11
-ERROR - 2012-03-27 00:47:57 --> Severity: Notice  --> A non well formed numeric value encountered /Users/admin/Sites/jobs/app/views/account/cases.php 11
-ERROR - 2012-03-27 01:43:18 --> Query error: Not unique table/alias: 'prices'
-ERROR - 2012-03-27 23:32:19 --> 404 Page Not Found --> assets
