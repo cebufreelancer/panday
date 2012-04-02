ca87adc8d5ef7f0fd955cf61aa4927e79fd7f49a
Mike <michaxze@gmail.com>
2012-04-02 23:38:42 +0800

    new file

---
+++ jobs/app/views/checkout.php
@@ -0,0 +1,5 @@
+<div class="page-header">
+  <h3>You need to have an account in order to buy cases.  </h3>
+  <p> <a class="label" href="/register">Register here</a> if you don't have an account. </p>
+  <p> <a class="label" href="/"> Login here if you already have an account </a></p>
+</div>
