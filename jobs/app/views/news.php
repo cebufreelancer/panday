ef21a9618c6fee4ecd464c0f135a029481357dd2
Mike <michaxze@gmail.com>
2012-05-01 23:36:51 +0800

    update

--- jobs/app/views/news.php
+++ jobs/app/views/news.php
@@ -1,6 +1,6 @@
 <div class="page-header">
   <h3><?php echo $new['title'];?></h3>
   <p> 
-  <img src="/assets/images/sample1.jpg" width="150" align="left" style="padding-right: 6px; padding-bottom: 6px;">
+  <img src="/assets/news/<?php echo $new['id'];?>/<?php echo $new['image'];?>" width="150" align="left" style="padding-right: 6px; padding-bottom: 6px;">
   <?php echo str_replace("\n", "<br/>", $new['contents']);?></p>
 </div>
